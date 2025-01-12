3a1edd0fd5a166f2dcea03e894d08ae9620b397b
Niek Melet <niek+git@meletstudios.com>
2025-01-12 19:26:24 +0100

    Commit for exercise

---
+++ Chapter 1/Ex1_01.cpp
@@ -0,0 +1,22 @@
+// Ex1_01.cpp - A complete C++ program
+
+/* @note - This doesn't work... */
+// import std; // This line makes the entire Standard Library available
+			// including the std::println() functionality used below
+
+/* Other solution */
+#include <print>
+
+int main()
+{
+	// Exercise 1-1: Print Hello World
+	std::println("Hello, World!");
+
+	// Exercise 1-2: Print my name and my age on another line
+	std::println("Niek");
+	std::println("20 years old");
+
+	// Exercise 1-3: Remove the '#' from "#import std"
+
+	return 0;
+}
