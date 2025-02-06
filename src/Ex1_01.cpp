6158d16c963108a1c881d4c49b0b0d3b0c5f01b7
Niek Melet <niek+git@meletstudios.com>
2025-02-06 13:34:56 +0100

    Changed name of executable

---
+++ src/Ex1_01.cpp
@@ -0,0 +1,9 @@
+// Ex1_01.cpp - A complete C++ program
+#include<print>
+
+int main()
+{
+	int answer{ 42 };
+	std::println("The answer to life, the universe and everything is {}.", answer);	
+	return 0;
+}
