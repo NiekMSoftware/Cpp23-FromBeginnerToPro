28cc72f5602474fdef48838cee1e401dd36fe663
Niek Melet <niek+git@meletstudios.com>
2025-02-06 13:41:30 +0100

    Finished exercises for chapter 1.

--- src/Ex1_01.cpp
+++ src/Ex1_01.cpp
@@ -1,9 +1,27 @@
 // Ex1_01.cpp - A complete C++ program
 #include<print>
 
-int main()
-{
-	int answer{ 42 };
-	std::println("The answer to life, the universe and everything is {}.", answer);	
+void exercise1() {
+	std::println("Hello, World!");
+}
+
+void exercise2() {
+	int age{ 20 };
+	const char* name{ "Niek" };
+
+	std::println("My name is {} and I am {} years old.", name, age);
+}
+
+/* For exercise 3:
+ *
+ * The # should be excluded from the first line (import std;)
+ * Variable "Int" should be lower-case instead.
+ *
+ * `std::printn` doesn't exist in the std library, `std::println()` should be used instead.
+ * Also, there appears to be no return statement or a ';' after the print.
+ */
+
+int main() {
+	exercise2();
 	return 0;
 }
